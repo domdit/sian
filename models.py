69db57a70fade8356d48b3e5a10d76d527f5ed93
Dominic DiTaranto <domdit@gmail.com>
2019-03-08 16:43:11 -0600

    just a quick test

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
 
     def __repr__(self):
-        return f"User('{self.email}')"
+        return "User('{self.email}')"
 
 
 class MenuItem(db.Model):
